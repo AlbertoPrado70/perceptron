7acebdc51b07db06be333f0fd64b72fd8579d816
Alberto Prado <danteprado.70@gmail.com>
2020-05-17 15:45:30 -0500

    Añadidendo separación entre neurona y poblacion

--- neuron.js
+++ neuron.js
@@ -1,5 +1,62 @@
-const INPUTS = 13;
-const OUTPUTS = 4;
-const HIDDEN_LAYERS = 2; 
-const HIDDEN_LAYER_SIZE = 7;  
+let inputs = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]; 
 
+let weights_1 = matrix(7, inputs.length);
+let bias_1 = [0, 0, 0, 0, 0, 0, 0] 
+let output_1 = [0, 0, 0, 0, 0, 0, 0];
+
+let weights_2 = matrix(7, 7);
+let bias_2 = [0, 0, 0, 0, 0, 0, 0] 
+let output_2 = [0, 0, 0, 0, 0, 0, 0];
+
+let output_weights = matrix(4, 7);
+let output_bias = [0, 0, 0, 0];
+let outputs = [0, 0, 0, 0]; 
+
+function makeMove() {
+
+    for(let row = 0; row < weights_1.length; row++) {
+        output_1[row] = dot(weights_1[row], inputs);
+        output_1[row] += bias_1[row];
+        output_1[row] = relu(output_1[row]);
+    }
+
+    for(let row = 0; row < weights_2.length; row++) {
+        output_2[row] = dot(weights_2[row], output_1);
+        output_2[row] += bias_2[row];
+        output_2[row] = relu(output_2[row]);
+    }
+
+    for(let row = 0; row < output_weights.length; row++) {
+        outputs[row] = dot(output_weights[row], output_2);
+        outputs[row] += output_bias[row];
+        outputs[row] = relu(outputs[row]);
+    }
+
+}
+
+function matrix(rows, columns) {
+
+    let new_matrix = [rows];
+
+    for(let i = 0; i < rows; i++) {
+        new_matrix[i] = [columns];
+        for(let j = 0; j < columns; j++) {
+            new_matrix[i][j] = (Math.random() * 2) - 1;
+        }
+    }
+
+    return(new_matrix);
+
+}
+
+function dot(a, b) {
+    return a.map((e, i) => e * b[i]).reduce((t, i) => t += i);
+}
+
+function relu(x) {
+    return (x >= 0) ? x : 0;
+}
+
+function sigmoid(t) {
+    return 1/(1+Math.pow(Math.E, -t));
+}
--- pacman.js
+++ pacman.js
@@ -5,7 +5,7 @@ const TILE_SIZE = 32;
 let fitness = 0;
 let player = {x: 1, y: 1}; 
 let last_position = {x: 1, y: 1}; 
-let ghost = [{x: 12, y: 13}, {x: 13, y: 13}];
+let ghost = [{x: 13, y: 13}, {x: 13, y: 13}];
 let direcciones = [{x: 0, y: -1}, {x: 1, y: 0}, {x: 0, y: 1}, {x: -1, y: 0}];
 
 let board = [
@@ -14,13 +14,13 @@ let board = [
     [1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1],
-    [1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1],
-    [1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1],
     [1, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1],
+    [1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1],
     [1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -59,15 +59,15 @@ app.ticker.add(delta => {
     graphics.drawRect(ghost[0].x * TILE_SIZE, ghost[0].y * TILE_SIZE + 16, TILE_SIZE, TILE_SIZE / 2);
     graphics.endFill();
     
-    // Fantasma morado
-    graphics.lineStyle(0, 0x000);
-    graphics.beginFill(0xff00ff, 1);
-    graphics.drawCircle(ghost[1].x * TILE_SIZE + 16, ghost[1].y * TILE_SIZE + 16, TILE_SIZE / 2);
-    graphics.drawRect(ghost[1].x * TILE_SIZE, ghost[1].y * TILE_SIZE + 16, TILE_SIZE, TILE_SIZE / 2);
-    graphics.endFill();
+    // // Fantasma morado
+    // graphics.lineStyle(0, 0x000);
+    // graphics.beginFill(0xff00ff, 1);
+    // graphics.drawCircle(ghost[1].x * TILE_SIZE + 16, ghost[1].y * TILE_SIZE + 16, TILE_SIZE / 2);
+    // graphics.drawRect(ghost[1].x * TILE_SIZE, ghost[1].y * TILE_SIZE + 16, TILE_SIZE, TILE_SIZE / 2);
+    // graphics.endFill();
 
-    // Movimiento de entrenamiento
-    movimientoRandom();
+    // // Movimiento de entrenamiento
+    // movimientoRandom();
 
 });
 
@@ -94,6 +94,29 @@ function movimientoRandom() {
 
     else {
         last_position = player;
+    }
+
+}
+
+// Posicion al azar
+function posicionRandom() {
+
+    player = {x: Math.round(Math.random() * (BOARD_SIZE - 1)), y: Math.round(Math.random() * (BOARD_SIZE - 1))}; 
+
+    while(board[player.y][player.x] == 1) {
+        player = {x: Math.round(Math.random() * (BOARD_SIZE - 1)), y: Math.round(Math.random() * (BOARD_SIZE - 1))};
+    }
+
+}
+
+// Mueve al fantasma si la posición es valida
+function moveGhost(dir) {
+    
+    next_x = ghost[0].x + direcciones[dir].x; 
+    next_y = ghost[0].y + direcciones[dir].y; 
+
+    if(board[next_y][next_x] == 0) {
+        ghost[0] = {x: next_x, y: next_y}; 
     }
 
 }
